c40450310affc611b6bdb186e15fb0734df29cf2
Julio Santos <julio958214@gmail.com>
2023-04-18 21:11:10 -0300

    test

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        tags = "@regression",
+        tags = "@app_version",
         features = {"src/test/resources/features"},
         plugin = {"json:target/cucumber.json"},
         glue = {"steps"},
